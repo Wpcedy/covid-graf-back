b53b9c316c585c95469e9598089c554e171711d9
Wpcedy <wpcedy@gmail.com>
2023-02-15 19:12:54 -0300

    Ajustes no redis

--- controllers/covid.js
+++ controllers/covid.js
@@ -1,11 +1,3 @@
-const redis = require('redis');
-const client = redis.createClient();
-client.connect();
-
-client.on("error", (error) => {
-    console.error(error);
-});
-
 const https = require('https');
 const axios = require('axios');
 
@@ -33,38 +25,31 @@ const regions = (req, res, next) => {
 };
 
 const report = async (req, res, next) => {
-    var keyRedis = req.query.region_name + '&' + req.query.iso
-    let covidReports = await client.get(`${keyRedis}`);
-    if (!covidReports) {
-        axios({
-            method: 'get',
-            url: url + '/reports?region_name=' + req.query.region_name + '&iso=' + req.query.iso,
-            headers: {
-                'X-RapidAPI-Key': key,
-                'X-RapidAPI-Host': host
-            }
-        }).then(async response => {
-            var responseReports = {
-                'confirmed': 0,
-                'deaths': 0,
-                'recovered': 0,
-                'active': 0
-            };
-            var response = response.data.data;
-            response.forEach(province => {
-                responseReports.confirmed += province.confirmed;
-                responseReports.deaths += province.deaths;
-                responseReports.recovered += province.recovered;
-                responseReports.active += province.active;
-            });
-            await client.set(`${keyRedis}`, JSON.stringify(responseReports));
-            res.json(responseReports);
-        }).catch(err => {
-            res.json({ message: 'Error: ' + err.message });
+    axios({
+        method: 'get',
+        url: url + '/reports?region_name=' + req.query.region_name + '&iso=' + req.query.iso,
+        headers: {
+            'X-RapidAPI-Key': key,
+            'X-RapidAPI-Host': host
+        }
+    }).then(async response => {
+        var responseReports = {
+            'confirmed': 0,
+            'deaths': 0,
+            'recovered': 0,
+            'active': 0
+        };
+        var response = response.data.data;
+        response.forEach(province => {
+            responseReports.confirmed += province.confirmed;
+            responseReports.deaths += province.deaths;
+            responseReports.recovered += province.recovered;
+            responseReports.active += province.active;
         });
-    } else {
-        res.json(JSON.parse(covidReports));
-    }
+        res.json(responseReports);
+    }).catch(err => {
+        res.json({ message: 'Error: ' + err.message });
+    });
 };
 
 module.exports = { regions, report };
